ca8b844fe3d570ff9761d80579603878beec7db6
jsopn <github@jsopn.com>
2023-10-09 16:18:07 +0700

    feat: spawn threads on new file created, wait for unlock faster, check for vrmonitor.exe

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
-use anyhow::Result;
-use std::path::{Path, PathBuf};
-use tempfile::tempdir;
-
+use anyhow::{anyhow, Result};
 use futures::{
     channel::mpsc::{channel, Receiver},
     SinkExt, StreamExt,
 };
 use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
 use serde_derive::{Deserialize, Serialize};
+use std::{
+    path::{Path, PathBuf},
+    process,
+};
+use sysinfo::{ProcessExt, System, SystemExt};
 use teloxide::{requests::Requester, types::InputFile, Bot};
 
 use tokio::fs;
@@ -19,7 +21,7 @@ use tokio::time::{sleep, Duration};
 
 #[derive(Default, Debug, Serialize, Deserialize)]
 struct Config {
-    path: String,
+    path: Vec<String>,
     token: String,
     chat_id: String,
 }
@@ -27,16 +29,9 @@ struct Config {
 #[tokio::main]
 async fn main() -> Result<()> {
     std::env::set_var("RUST_LOG", "INFO");
-    pretty_env_logger::init();
+    pretty_env_logger::init_timed();
 
     let cfg: Config = confy::load_path(Path::new("./config.toml"))?;
-    if cfg.path == "" {
-        error!("please specify a path");
-
-        return Ok(());
-    }
-
-    info!("watching {}", cfg.path);
 
     futures::executor::block_on(async {
         if let Err(e) = async_watch(cfg).await {
@@ -62,33 +57,85 @@ fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Resul
     Ok((watcher, rx))
 }
 
+async fn read_unlocked(path: PathBuf) -> Result<Vec<u8>> {
+    let mut data: Option<Vec<u8>> = None;
+    for _ in 0..100 {
+        match fs::read(&path).await {
+            Ok(d) => {
+                data = Some(d);
+                break;
+            }
+            Err(e) => {
+                sleep(Duration::from_millis(50)).await;
+
+                trace!("error when trying to open file: {:?}; err: {}", path, e);
+
+                continue;
+            }
+        }
+    }
+
+    if let Some(value) = data {
+        Ok(value)
+    } else {
+        Err(anyhow!("failed to open file: {:?}", path))
+    }
+}
+
 async fn upload_file(path: PathBuf, chat_id: String, token: String) -> Result<()> {
-    info!("screenshot taken: {:?}", path);
+    let file_name = path.file_name().unwrap().to_owned();
+    info!("screenshot taken: {:?}", file_name);
 
-    sleep(Duration::from_millis(1500)).await;
-
-    let temp_dir = tempdir()?;
-    let temp_screenshot = Path::join(temp_dir.path(), "screenshot.png");
-    fs::copy(path, &temp_screenshot).await?;
-
-    let data = fs::read(temp_screenshot).await?;
     let bot = Bot::new(token);
+    let data = read_unlocked(path).await?;
 
     bot.send_photo(chat_id, InputFile::memory(data)).await?;
-
-    fs::remove_dir_all(temp_dir).await?;
     drop(bot);
 
-    info!("screenshot uploaded!");
+    info!("screenshot uploaded!: {:?}", file_name);
 
     Ok(())
 }
 
+async fn monitor_processes() {
+    let mut monitor_found: Option<bool> = None;
+
+    'l: loop {
+        let s = System::new_all();
+        for _ in s.processes_by_name("vrmonitor") {
+            monitor_found = Some(true);
+            continue 'l;
+        }
+
+        if monitor_found.is_none() {
+            warn!("no vrmonitor.exe process was found during startup, program will not close when you exit VR");
+
+            break;
+        }
+
+        monitor_found = Some(false);
+
+        if let Some(v) = monitor_found {
+            if v == false {
+                warn!("vrmonitor.exe was closed");
+
+                process::exit(0);
+            }
+        }
+    }
+}
+
 async fn async_watch(cfg: Config) -> Result<()> {
     let (mut watcher, mut rx) = async_watcher()?;
-    let path = Path::new(&cfg.path);
 
-    watcher.watch(path, RecursiveMode::Recursive)?;
+    tokio::spawn(async { monitor_processes().await });
+
+    for path in cfg.path {
+        info!("watching {}", &path);
+
+        let path = Path::new(&path);
+        watcher.watch(path, RecursiveMode::Recursive)?;
+    }
 
     while let Some(res) = rx.next().await {
         match res {
@@ -98,10 +145,11 @@ async fn async_watch(cfg: Config) -> Result<()> {
                 }
 
                 for path in event.paths {
-                    match upload_file(path, cfg.chat_id.to_string(), cfg.token.to_string()).await {
-                        Ok(()) => (),
-                        Err(e) => error!("upload error: {:?}", e),
-                    };
+                    tokio::spawn(upload_file(
+                        path,
+                        cfg.chat_id.to_string(),
+                        cfg.token.to_string(),
+                    ));
                 }
             }
             Err(e) => error!("watch error: {:?}", e),
